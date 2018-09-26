46c1810cdc63b11c5b8a437d5350e3a2e02952eb
Tim Ferro <timferro@gmail.com>
2018-09-25 19:54:57 -0500

    mult

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,26 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+// Adds 1+...+100.
+@i // i refers to some mem. location.
+M=1 // i=1
+@R2 // sum refers to some mem. location.
+M=0 // sum=0
+(LOOP)
+@i
+D=M // D=i
+@R1
+D=D-M // D=i-R1
+@END
+D;JGT // If (i-R1)>0 goto END
+@R0
+D=M // D=R0
+@R2
+M=D+M // R2=R2+R0
+@i
+M=M+1 // i=i+1
+@LOOP
+0;JMP // Goto LOOP
+(END)
+@END
+0;JMP // Infinite loop
