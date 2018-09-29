e4d04defe616b39f071e73cc9cdfa585fd1cc341
Tim Ferro <timferro@gmail.com>
2018-09-29 10:50:47 -0500

    fill

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,40 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(LOOP)
+@KBD
+D=M // D = current key
+@currkey
+M=D // store current key
+@R1
+M=0 // default fill is 000000000000000
+@currkey
+D=M // D = current key
+@DOFILL
+D;JEQ // if no key goto DOFILL
+@R1
+M=-1 // set fill to 1111111111111111
+(DOFILL)
+@SCREEN
+D=A
+@offsetptr
+M=D // init offsetptr to screen address
+(FILLLOOP)
+@KBD
+D=A // keyboard offset (end of screen memory)
+@offsetptr
+D=D-M // distance from offset to end of screen memory
+@NEXT
+D;JEQ // exit to keyboard loop at end of screen memory
+@R1
+D=M // load fill
+@offsetptr
+A=M // dereference offsetptr
+M=D // set fill at offset in screen memory
+@offsetptr
+M=M+1 // increment offsetptr
+@FILLLOOP
+0;JMP
+(NEXT)
+@LOOP
+0;JMP
